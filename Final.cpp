885cab9558a3005a6654dfb4c47bb01b65b57e5b
naredokitsch <naredokitsch@utexas.edu>
2018-06-08 18:17:24 -0500

    first commit

---
+++ Final.cpp
@@ -0,0 +1,209 @@
+//------------  Semestre 2018-2 --------------------//
+//----------------  Examen Extraordinario -----------//
+//------ALUMNO:									---//
+
+#include "texture.h"
+#include "figuras.h"
+#include "Camera.h"
+
+
+int w = 500, h = 500;
+
+CCamera objCamera;	//Create objet Camera
+
+GLfloat g_lookupdown = 0.0f;    // Look Position In The Z-Axis (NEW) 
+
+
+CTexture text1;
+CFiguras fig1;
+
+			
+void InitGL ( GLvoid )     // Inicializamos parametros
+{
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);				// Negro de fondo	
+
+	glEnable(GL_TEXTURE_2D);
+	glShadeModel (GL_SMOOTH);
+	
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+
+	glClearDepth(1.0f);									// Configuramos Depth Buffer
+	glEnable(GL_DEPTH_TEST);							// Habilitamos Depth Testing
+	glDepthFunc(GL_LEQUAL);								// Tipo de Depth Testing a realizar
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+
+	glEnable(GL_AUTO_NORMAL);
+	glEnable(GL_NORMALIZE);
+    
+    text1.LoadBMP("Texturas/01.bmp");
+	text1.BuildGLTexture();
+	text1.ReleaseImage();
+
+	objCamera.Position_Camera(0,2.5f,3, 0,2.5f,0, 0, 1, 0);
+
+}
+
+
+
+void display ( void )   // Creamos la funcion donde se dibuja
+{
+	glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	
+	glLoadIdentity();
+	
+		glRotatef(g_lookupdown,1.0f,0,0);
+
+		gluLookAt(	objCamera.mPos.x,  objCamera.mPos.y,  objCamera.mPos.z,	
+					objCamera.mView.x, objCamera.mView.y, objCamera.mView.z,	
+					objCamera.mUp.x,   objCamera.mUp.y,   objCamera.mUp.z);
+	
+
+		glPushMatrix();		
+			glPushMatrix(); //Creamos cielo
+				glDisable(GL_LIGHTING);
+				glTranslatef(0,60,0);
+				glColor3f(1.0, 1.0, 1.0);
+				fig1.skybox(130.0, 130.0, 130.0,text1.GLindex);
+			glPopMatrix();
+
+			glPushMatrix();
+				glLineWidth(2.0);
+				glBegin(GL_LINES); //Ejes de Referencia
+					glColor3f(1.0, 0.0, 0.0);
+					glVertex3f(0.0, 0.0, 0.0);
+					glVertex3f(80.0, 0.0, 0.0);
+					glColor3f(0.0, 1.0, 0.0);
+					glVertex3f(0.0, 0.0, 0.0);
+					glVertex3f(0.0, 80.0, 0.0);
+					glColor3f(0.0, 0.0, 1.0);
+					glVertex3f(0.0, 0.0, 0.0);
+					glVertex3f(0.0, 0.0, 80.0);
+				glEnd();
+				glEnable(GL_LIGHTING);
+
+			glPopMatrix();
+		glPopMatrix();
+
+		//Poner código de examen apartir de este punto
+		
+
+	glutSwapBuffers ( );
+
+}
+
+
+void reshape ( int width , int height )   // Creamos funcion Reshape
+{
+  if (height==0)										// Prevenir division entre cero
+	{
+		height=1;
+	}
+
+	glViewport(0,0,width,height);	
+
+	glMatrixMode(GL_PROJECTION);						// Seleccionamos Projection Matrix
+	glLoadIdentity();
+
+	// Tipo de Vista
+	
+	glFrustum (-0.1, 0.1,-0.1, 0.1, 0.1, 170.0);
+
+	glMatrixMode(GL_MODELVIEW);							// Seleccionamos Modelview Matrix
+	glLoadIdentity();
+}
+
+void animacion()
+{
+	//Colocar aqui codigo de animacion
+
+
+
+	glutPostRedisplay();
+}
+
+void keyboard ( unsigned char key, int x, int y )  // Create Keyboard Function
+{
+	switch ( key ) {
+
+		case 'w':   //Movimientos de camara
+		case 'W':
+			objCamera.Move_Camera( CAMERASPEED+0.2 );
+			break;
+
+		case 's':
+		case 'S':
+			objCamera.Move_Camera(-(CAMERASPEED+0.2));
+			break;
+
+		case 'a':
+		case 'A':
+			objCamera.Strafe_Camera(-(CAMERASPEED+0.4));
+			break;
+
+		case 'd':
+		case 'D':
+			objCamera.Strafe_Camera( CAMERASPEED+0.4 );
+			break;
+
+		case 27:        // Cuando Esc es presionado...
+			exit ( 0 );   // Salimos del programa
+			break;        
+		default:        // Cualquier otra
+			break;
+  }
+
+  glutPostRedisplay();
+}
+
+void arrow_keys ( int a_keys, int x, int y )  // Funcion para manejo de teclas especiales (arrow keys)
+{
+  switch ( a_keys ) {
+	case GLUT_KEY_PAGE_UP:
+		objCamera.UpDown_Camera(CAMERASPEED);
+		break;
+
+	case GLUT_KEY_PAGE_DOWN:
+		objCamera.UpDown_Camera(-CAMERASPEED);
+		break;
+
+    case GLUT_KEY_UP:     // Presionamos tecla ARRIBA...
+		g_lookupdown -= 1.0f;
+		break;
+
+    case GLUT_KEY_DOWN:               // Presionamos tecla ABAJO...
+		g_lookupdown += 1.0f;
+		break;
+
+	case GLUT_KEY_LEFT:
+		objCamera.Rotate_View(-CAMERASPEED);
+		break;
+
+	case GLUT_KEY_RIGHT:
+		objCamera.Rotate_View( CAMERASPEED);
+		break;
+
+    default:
+		break;
+  }
+  glutPostRedisplay();
+}
+
+int main ( int argc, char** argv )   // Main Function
+{
+  glutInit            (&argc, argv); // Inicializamos OpenGL
+  glutInitDisplayMode (GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH); // Display Mode (Clores RGB y alpha | Buffer Doble )
+  glutInitWindowSize  (800, 800);	// Tamaño de la Ventana
+  glutInitWindowPosition (0, 0);	//Posicion de la Ventana
+  glutCreateWindow    ("Extraordinario"); // Nombre de la Ventana
+  //glutFullScreen     ( );         // Full Screen
+  InitGL ();						// Parametros iniciales de la aplicacion
+  glutDisplayFunc     ( display );  //Indicamos a Glut función de dibujo
+  glutReshapeFunc     ( reshape );	//Indicamos a Glut función en caso de cambio de tamano
+  glutKeyboardFunc    ( keyboard );	//Indicamos a Glut función de manejo de teclado
+  glutSpecialFunc     ( arrow_keys );	//Otras
+  glutIdleFunc	      (animacion);
+  glutMainLoop        ( );          // 
+
+  return 0;
+}
---
+++ figuras.h
@@ -0,0 +1,15 @@
+//#include <windows.h>  //Solo para Windows
+//#include <GL/gl.h>     // The GL Header File
+//#include <stdio.h>
+
+//#include <math.h>
+#include "Main.h"
+
+
+class CFiguras
+{
+	public:
+
+	void skybox(float altura, float largo, float profundidad, GLuint text);	//Funcion creacion cielo
+
+};
